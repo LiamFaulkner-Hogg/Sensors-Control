216a0b38a21eddbb1c17caae255bf578b37a8670
Ubuntu <student@labMGJSX0.dibj4albw4huhoiqswueuf4m3d.px.internal.cloudapp.net>
2024-10-27 11:37:54 +0000

    update

---
+++ build/CMakeFiles/SandC_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SandC_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/SandC_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/gopro_publisher.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gopro_publisher.dir/src/gopro_publisher.cpp.o"
+  "CMakeFiles/gopro_publisher.dir/src/gopro_publisher.cpp.o.d"
+  "gopro_publisher"
+  "gopro_publisher.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gopro_publisher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
